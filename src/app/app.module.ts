26c2e378c12187cebb40b3be5a7cc166ed569082
Maik Kreutzer <kreutzermaik123@web.de>
2020-12-19 18:49:30 +0100

    Übersicht-Ansicht hinzugefuegt, Backend aufgesetzt für Rekorde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { CsvComponent } from './csv/csv.component';
+import { OverviewComponent } from './overview/overview.component';
+import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: MainComponent },
+  { path: "overview", component: OverviewComponent },
+  { path: "csv", component: CsvComponent },
+  { path: "**", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CsvComponent,
+    OverviewComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
